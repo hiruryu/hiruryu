479227c9e399ede90207155a3fc8195f28608208
みゅる <hiruryu02@gmail.com>
2026-03-06 18:47:47 +0900

    Update aRules.js

--- docs/conlang/cdic/rules/aRules.js
+++ docs/conlang/cdic/rules/aRules.js
@@ -1,19 +1,30 @@
-// スマートな形容詞屈折形生成
 function getConjA(word, stem, type, ruletype) {
   if (ruletype === "no") return {};
 
   const rules = {
-    c0: {
+    t: {
       suffixes: {
-        s: "ui", s2: "onna", s3: "uiska",
-        fs: "afi", fs2: "afna", fs3: "afuiska",
+        s: "e", s2: "onna", s3: "uiska",
+        fs: "afie", fs2: "afna", fs3: "afuiska",
+        on: "ashie", fs2: "afna", fs3: "afuiska",
         es: "ol", es2: "onna", es3: "oluiska",
         ds: "ies", ds2: "ienna", ds3: "iesska",
         ads: "iup", ads2: "iupnui", ads3: "iupuiska",
-        nar: "irus"
+        nar: "os"
+      }
+    },
+    k: {
+      suffixes: {
+        s: "e", s2: "onna", s3: "uiska",
+        fs: "afie", fs2: "afna", fs3: "afuiska",
+        on: "ashie", fs2: "afna", fs3: "afuiska",
+        es: "ol", es2: "onna", es3: "oluiska",
+        ds: "ies", ds2: "ienna", ds3: "iesska",
+        ads: "iup", ads2: "iupnui", ads3: "iupuiska",
+        nar: "os"
       }
     }
-    // 他の ruletype があればここに追加可能
+    // 他の ruletype があればここに追加
   };
 
   const rule = rules[ruletype];
@@ -26,3 +37,4 @@ function getConjA(word, stem, type, ruletype) {
 
   return forms;
 }
+
